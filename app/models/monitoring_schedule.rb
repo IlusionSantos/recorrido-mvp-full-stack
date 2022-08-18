d33f62df2e1655e4d0d9ffa8613dcef43e89044a
IlusionSantos <ilusion.santosm@gmail.com>
2022-08-18 17:02:19 -0600

    Allow to create without users_id

--- app/models/monitoring_schedule.rb
+++ app/models/monitoring_schedule.rb
@@ -1,4 +1,4 @@
 class MonitoringSchedule < ApplicationRecord
   belongs_to :monitoring_service, class_name: 'MonitoringService', foreign_key: 'monitoring_services_id'
-  belongs_to :user, class_name: 'User', foreign_key: 'users_id'
+  belongs_to :user, class_name: 'User', foreign_key: 'users_id', optional: true
 end
